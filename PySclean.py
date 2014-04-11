c47e7a6b01cd45719573654f22c440fdcabd3500
David Xu <mysolowing@gmail.com>
2014-04-11 11:59:00 -0500

    Added remove result files

--- PySclean.py
+++ PySclean.py
@@ -3,18 +3,18 @@ Created on Apr 11, 2014
 @author: David Xu
 '''
 import os, re, sys
-
 print sys.argv[1:]
 argc=len(sys.argv)
 # argc=2
-types=[r"\w{1,}\.((ic)|(st)|(op)|(pa))[0-9]{1,}\w{0,}",r"\w{1,}.pyc", \
-       r"scope.log\w{0,}",r"\w{1,}\.\w{1,}~"]
-
+types=[r"\w{1,}\.((ic)|(st)|(op)|(pa)|(su))[0-9]{1,}\w{0,}",r"\w{1,}.pyc", \
+       r"scope.log\w{0,}",r"\w{1,}\.\w{1,}~",r"logFile"] # aux files
+types2=[r"\w{1,}\.((sw)|(tr)|(ac)|(mt))[0-9]{1,}\w{0,}"] # results files
+# types=types+types2  # include result files
 def tocheckexist(fname):
   for ftype in types:
     g=re.search(ftype, fname)
     if g is not None:
-      #print "file found ",g.group(0), "    ", ftype, "    ", fname
+      #print "found ",g.group(0), "  ", ftype, "  ", fname
       return ftype
   return None
 
